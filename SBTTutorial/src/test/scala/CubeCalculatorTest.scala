4cef45fd994a443ab790ffe5df550842dcdfe63b
MJRP <mjrp11@gmail.com>
2023-08-28 12:04:44 +0100

    Add unit tests

---
+++ SBTTutorial/src/test/scala/CubeCalculatorTest.scala
@@ -0,0 +1,25 @@
+import example.CubeCalculator
+import org.scalatest.Inspectors.forAll
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+
+class CubeCalculatorTest extends AnyFunSuite with Matchers:
+  test("Cube of 3 static value") {
+    assert(CubeCalculator.cube(3) === 27)
+  }
+
+  test("Cube of 2 compare with math.pow") {
+    assert(CubeCalculator.cube(2) === scala.math.pow(2,3))
+  }
+
+  forAll(MyTestsFactory.testCases) {
+    x => {
+      test(s"Test Dynamic Tests. Input $x") {
+        assert(CubeCalculator.cube(x) === scala.math.pow(x,3))
+      }
+    }
+  }
+
+  object MyTestsFactory {
+    val testCases: Seq[Int] = Seq(1, 2, 5, 8)
+  }
